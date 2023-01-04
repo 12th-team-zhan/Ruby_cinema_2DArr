05a32fce7883ea5fa3a0a9ca4fb5e1f7973931f1
keenchang <s1277865@gmail.com>
2023-01-04 13:00:20 +0800

    修改專案名稱

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Ruby_cinema <Ruby@cinema.com>'
+  config.mailer_sender = 'Ruby_cinema_2DArr <Ruby@cinema.com>'
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
 
