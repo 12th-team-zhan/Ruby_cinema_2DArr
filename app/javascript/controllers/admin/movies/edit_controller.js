dd4b42a69ae655d658d4fffcd194ce1aa15e8a38
keenchang <s1277865@gmail.com>
2023-01-06 21:29:22 +0800

    修改admin/movie/new的checkbox；修改admin/edit的JS、HTML

---
+++ app/javascript/controllers/admin/movies/edit_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  connect() {
+    this.setupTrix();
+  }
+  setupTrix() {
+    Trix.config.textAttributes.fontSize = {
+      styleProperty: "font-size",
+      inheritable: 1,
+    };
+
+    this.trix = this.element.querySelector("trix-editor");
+  }
+}
